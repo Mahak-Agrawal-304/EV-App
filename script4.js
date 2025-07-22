2223d74b3a0134eefaf1b9989c8f4c3b1a573130
Mahak Agrawal <132470319+Mahak-Agrawal-304@users.noreply.github.com>
2025-07-23 02:10:27 +0530

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,36 @@
+
+document.querySelectorAll('.slider5').forEach(carousel => new Splide( carousel, {
+// Desktop on down
+	perPage: 2,
+	perMove: 1,
+  focus: 0, // 0 = left and 'center' = center
+  type: 'slide', // 'loop' or 'slide'
+  gap: '31px', // space between slides
+  arrows: 'slider', // 'slider' or false
+  pagination: false, // 'slider' or false
+  speed : 600, // transition speed in miliseconds
+  dragAngleThreshold: 30, // default is 30
+  autoWidth: false, // for cards with differing widths
+  rewind : false, // go back to beginning when reach end
+arrowPath: 'm7.61 0.807-2.12...',
+  rewindSpeed : 400,
+  waitForTransition : false,
+  updateOnMove : true,
+  trimSpace: true, // true removes empty space from end of list
+  breakpoints: {
+		991: {
+    	// Tablet
+      pagination: false
+		},
+    767: {
+    	// Mobile Landscape
+      	perPage: 1,
+        pagination: false
+		},
+    479: {
+    	// Mobile Portrait
+        perPage: 1,
+        pagination: false
+		}
+	}
+} ).mount());
